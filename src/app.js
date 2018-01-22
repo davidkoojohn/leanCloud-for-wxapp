c99bededae10f35f3b41592c64aafeb4196e9e1c
davidkoojohn <davidkoojohn@gmail.com>
2018-01-22 10:00:39 +0800

    creat todos page

--- src/app.js
+++ src/app.js
@@ -1,4 +1,14 @@
-//app.js
+
+const AV = require('./utils/av-weapp-min')
+
+AV.init({
+  appId: 'bKRS3V9qtnKnGI8tRNISjfMp-gzGzoHsz',
+  appKey: '5aEPRwQPMUGCYl2oUY7f2Naw',
+});
+
+console.log(AV)
+
+
 App({
   onLaunch: function () {
     // 展示本地存储能力
---
+++ src/pages/todos/todos.js
@@ -0,0 +1,8 @@
+
+
+Page({
+  data: {
+    test: 'todos'
+  }
+})
+
